4cde9b548cbbc8df4453f03ec3366ff0437036bf
samsam4657 <146444241+samsam4657@users.noreply.github.com>
2023-10-03 14:21:23 +0200

    Update loop.sh
    Added titles

--- loop.sh
+++ loop.sh
@@ -13,15 +13,26 @@ do
     bowtie2 --threads 5 -k 100 -x ~/course/data/shared/mapping/db/aegenomics.db -U $file.vs.fq.gz --no-unal | samtools view -bS - > $file.bam
 done
 
+# Sort bam files with samtools
 conda activate day2
 for f in *.fq.gz.bam; do samtools sort -n ${f} -@ 5 > ${f}.sort.bam; done
-for f in *.sort.bam; do metaDMG-cpp lca -bam ${file} -names ~/course/data/shared/mapping/taxonomy/names.dmp -nodes ~/course/data/shared/mapping/taxonomy/nodes.dmp -acc2tax ~/course/data/shared/mapping/taxonomy/acc2taxid.map.gz -weighttype 1 -fix_ncbi 0 -out ${f}; done
+
+# Do lca
 conda activate metaDMG
+for f in *.sort.bam; do metaDMG-cpp lca -bam ${file} -names ~/course/data/shared/mapping/taxonomy/names.dmp -nodes ~/course/data/shared/mapping/taxonomy/nodes.dmp -acc2tax ~/course/data/shared/mapping/taxonomy/acc2taxid.map.gz -weighttype 1 -fix_ncbi 0 -out ${f}; done
+
+# Create config file
 metaDMG config *.sort.bam --names ~/course/data/shared/mapping/taxonomy/names.dmp --nodes ~/course/data/shared/mapping/taxonomy/nodes.dmp --acc2tax ~/course/data/shared/mapping/taxonomy/acc2taxid.map.gz -m /usr/local/bin/metaDMG-cpp
 vim config.yaml
+# Edit config with chose parameters (view in script)
+
+# Compute
 metaDMG compute config.yaml 
+
+# View on Dashboard
 metaDMG dashboard config.yaml
 
+# LCA Loop Isolated
 for f in *.sort.bam;
 do metaDMG-cpp lca -bam $f -names ~/course/data/shared/mapping/taxonomy/names.dmp -nodes ~/course/data/shared/mapping/taxonomy/nodes.dmp -acc2tax ~/course/data/shared/mapping/taxonomy/
 acc2taxid.map.gz -weighttype 1 -fix_ncbi 0 -out $f.lca;
