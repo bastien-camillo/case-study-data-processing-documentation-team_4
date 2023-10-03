f11b4b2af89095454c3fe776c8ce43520eb8395c
Bastien Camillo <97998498+bastien-camillo@users.noreply.github.com>
2023-10-03 13:33:44 +0200

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -12,3 +12,12 @@ do
     zcat ${file}.vs.fq.gz | awk '{if(NR%4==2) print length($1)}' | sort -n | uniq -c > $file.vs.fq.gz.read_length.txt
     bowtie2 --threads 5 -k 100 -x ~/course/data/shared/mapping/db/aegenomics.db -U $file.vs.fq.gz --no-unal | samtools view -bS - > $file.bam
 done
+
+conda activate day2
+for f in *.fq.gz.bam; do samtools sort -n ${f}.bam -@ 5 > ${f}.sort.bam; done
+for f in *.sort.bam; do metaDMG-cpp lca -bam ${file} -names ~/course/data/shared/mapping/taxonomy/names.dmp -nodes ~/course/data/shared/mapping/taxonomy/nodes.dmp -acc2tax ~/course/data/shared/mapping/taxonomy/acc2taxid.map.gz -weighttype 1 -fix_ncbi 0 -out ${f}; done
+conda activate metaDMG
+metaDMG config *.sort.bam --names ~/course/data/shared/mapping/taxonomy/names.dmp --nodes ~/course/data/shared/mapping/taxonomy/nodes.dmp --acc2tax ~/course/data/shared/mapping/taxonomy/acc2taxid.map.gz -m /usr/local/bin/metaDMG-cpp
+vim config.yaml
+metaDMG compute config.yaml 
+metaDMG dashboard config.yaml
